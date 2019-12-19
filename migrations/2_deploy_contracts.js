aa534a1a63191be322651aade9bd0993abb5dfbd
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2019-12-19 09:16:50 -0800

    rm unneccessary misc migrations/test logic

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,5 @@
-const TruffleLogger = artifacts.require("./TruffleLogger.sol");
 const MyDapp = artifacts.require("./MyDapp.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(TruffleLogger);
   deployer.deploy(MyDapp);
 };
--- test/LogTest.js
+++ test/LogTest.js
@@ -11,28 +11,5 @@ contract("LogTest", accounts => {
     const logEvts = tx.logs.filter(x => x.event === "_TruffleLog");
     console.log("No. of detected _TruffleLog events: ", logEvts.length);
 
-    // For each __TLog event
-    logEvts.forEach(evt => {
-      const result = evt.args;
-
-      // A mapping to show how to output into our console
-      const outputMap = {
-        num: result[0].toString(),
-        boolean: result[0],
-        str: result[0],
-        b32: result[0],
-        addr: result[0],
-      };
-
-      // A list of all "supported" types: ["num", "boolean", "str", ...]
-      const types = Object.keys(outputMap);
-
-      // Find out what type our result is, and log it out if it matches
-      types.forEach(type => {
-        if (result.hasOwnProperty(type)) {
-          console.log(outputMap[type]);
-        }
-      });
-    });
   });
 });
