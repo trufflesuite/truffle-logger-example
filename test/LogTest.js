cb3392d795c7123f3269e07d8ac627cdc979f1b8
Adrian Li <li.adrianmc@gmail.com>
2020-01-16 14:51:20 +0900

    add mechanism to catch named log events

--- test/LogTest.js
+++ test/LogTest.js
@@ -1,3 +1,4 @@
+const { toAscii } = require("web3-utils");
 const MyDapp = artifacts.require("./MyDapp.sol");
 
 contract("LogTest", accounts => {
@@ -7,9 +8,31 @@ contract("LogTest", accounts => {
     // Execute a transaction
     const tx = await myDapp.doSomething({ from: accounts[0] });
 
-    // Take all events, filter for our special __Log event
-    const logEvts = tx.logs.filter(x => x.event === "_TruffleLog");
-    console.log("No. of detected _TruffleLog events: ", logEvts.length);
+    // Catch log events with labels
+    const logNamedEvts = tx.logs.filter(x => x.event === "_TruffleLogNamed");
+    logNamedEvts.forEach(event => {
+      const result = event.args;
 
+      // A mapping to show how to output into our console
+      const outputMap = {
+        num: result[1].toString(),
+        boolean: result[1],
+        str: result[1],
+        b32: result[1],
+        addr: result[1],
+      };
+
+      // A list of all "supported" types: ["num", "boolean", "str", ...]
+      const types = Object.keys(outputMap);
+
+      // Find out what type our result is, and log it out if it matches
+      types.forEach(type => {
+        if (result.hasOwnProperty(type)) {
+          const label = toAscii(result[0]);
+          const val = outputMap[type];
+          console.log(label, val);
+        }
+      });
+    });
   });
 });
